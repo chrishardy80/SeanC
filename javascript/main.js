2494e5435d273b0757582009006bd32e6ff3ace5
Chris Hardy <chrishardy@Chriss-MBP.home>
2019-06-02 11:56:12 +0100

    first commit.

---
+++ javascript/main.js
@@ -0,0 +1,60 @@
+function main () {
+ $(".Two").hide();
+
+
+anime({
+targets: '.rightarrow',
+translateX: 40,
+opacity: 1,
+easing: 'easeInOutQuad',
+duration: 1000
+});
+
+$(".rightarrow").click(function() {
+  anime({
+  targets: '.rightarrow',
+  translateX: 80,
+  opacity: 0,
+  easing: 'easeInOutQuad',
+  duration: 1000
+  });
+
+  anime({
+  targets: '.leftarrow',
+  translateX: -40,
+  opacity: 1,
+  easing: 'easeInOutQuad',
+  duration: 1000
+  });
+
+  $(".One").hide( "blind", {direction: "right", easing: "easeOutQuad"}, 500,function(){
+    $(".Two").show( "blind", {direction: "right", easing: "easeOutQuad"}, 500);
+  });
+
+});
+
+$(".leftarrow").click(function() {
+  anime({
+  targets: '.rightarrow',
+  translateX: 40,
+  opacity: 1,
+  easing: 'easeInOutQuad',
+  duration: 1000
+  });
+
+  anime({
+  targets: '.leftarrow',
+  translateX: 0,
+  opacity: 0,
+  easing: 'easeInOutQuad',
+  duration: 1000
+  });
+
+  $(".Two").hide( "blind", {direction: "left", easing: "easeOutQuad"}, 500,function(){
+  $(".One").show( "blind", {direction: "left", easing: "easeOutQuad"}, 500);
+  });
+
+});
+}
+
+$(document).ready(main);
---
+++ javascript/scroll.js
@@ -0,0 +1,192 @@
+$(document).ready(function() {
+
+
+var fade = document.querySelector("#fade");
+var title = document.querySelector("#title");
+var filler = document.querySelector("#filler");
+var bImage = document.querySelector("#backgroundimage");
+var vHeader = document.querySelector("header");
+var about = document.querySelector("#about");
+var closer = document.querySelector("#closer");
+var paragraphs = document.querySelector(".mainparagraphs")
+var copyright = document.querySelector("#copyright");
+var links = document.querySelector("#links");
+
+var imageStyle = getComputedStyle(bImage);
+var headerStyle = getComputedStyle(vHeader);
+var titleStyle = getComputedStyle(title);
+var aboutStyle = getComputedStyle(about);
+var closerStyle = getComputedStyle(closer);
+var paragraphStyle = getComputedStyle(paragraphs);
+var copyrightStyle = getComputedStyle(copyright);
+var linksStyle = getComputedStyle(links)
+
+var imageWidth = imageStyle.width;
+var headerWidth = headerStyle.width;
+var headerHeight = headerStyle.height;
+var imageMargin = imageStyle.marginRight;
+var titleMargin = titleStyle.paddingLeft;
+var titleWidth = titleStyle.width;
+var aboutMargin = aboutStyle.marginRight;
+var aboutMarginTop = aboutStyle.marginTop;
+var aboutMarginBottom = aboutStyle.marginBottom;
+var aboutFontSize = aboutStyle.fontSize;
+var aboutHeight = aboutStyle.height;
+var closerHeight = closerStyle.height;
+var paragraphsHeight = paragraphStyle.height;
+var linksHeight = linksStyle.height;
+
+var imageWidthValue = imageWidth.slice(0, -2);
+var imageMarginValue = imageMargin.slice(0, -2);
+var headerWidthValue = headerWidth.slice(0, -2);
+var headerHeightValue = headerHeight.slice(0, -2);
+var titleMarginValue = titleMargin.slice(0, -2);
+var titleWidthValue = titleWidth.slice(0, -2);
+var aboutMarginValue = aboutMargin.slice(0, -2);
+var aboutMarginTopValue = aboutMarginTop.slice(0, -2);
+var aboutMarginBottomValue = aboutMarginBottom.slice(0, -2);
+var aboutFontSizeValue = aboutFontSize.slice(0, -2);
+var aboutHeightValue = aboutHeight.slice(0, -2);
+var closerHeightValue = closerHeight.slice(0, -2);
+var paragraphsHeightValue = paragraphsHeight.slice(0, -2);
+var linksHeightValue = linksHeight.slice(0, -2);
+
+if($(window).width()>990)
+{
+ window.addEventListener('load', function() {
+
+fade.style.width =  (parseFloat(imageWidthValue)*1.2) + "px";
+filler.style.width = imageMargin;
+title.style.width = (parseFloat(headerWidthValue) - parseFloat(titleMarginValue) - parseFloat(imageWidthValue) - parseFloat(imageMarginValue)) + "px" ;
+about.style.width = (headerWidthValue - titleMarginValue - imageWidthValue - imageMarginValue - aboutMarginValue) + "px" ;
+links.style.height = ($(window).height()*0.0852) + "px";
+
+var aboutArea = (parseFloat(headerWidthValue) - parseFloat(titleMarginValue) - parseFloat(imageWidthValue) - parseFloat(imageMarginValue) - parseFloat(aboutMarginValue))*($(window).height() - parseFloat(headerHeightValue) - parseFloat(aboutMarginTopValue) - parseFloat(aboutMarginBottomValue) - parseFloat(closerHeightValue));
+copyright.style.marginRight = (parseFloat(imageWidthValue) + parseFloat(imageMarginValue) + 30) + "px";
+about.style.height = ($(window).height() - parseFloat(headerHeightValue) - parseFloat(aboutMarginTopValue) - parseFloat(aboutMarginBottomValue) - parseFloat(closerHeightValue)) + "px";
+if (aboutArea < 291800)
+{
+  if (Math.floor(aboutArea*0.00007) > 14)
+  {
+  about.style.fontSize = (Math.floor(aboutArea*0.00007)) + "px";
+  }
+  else {
+    about.style.fontSize = "14px";
+  }
+}
+else
+{
+  about.style.fontSize = "20px";
+}
+
+if (titleWidthValue > 500)
+  {
+    title.style.fontSize = "100px";
+  }
+else
+  {
+    title.style.fontSize = (titleWidthValue*0.175) + "px";
+  }
+
+});
+}
+
+
+
+
+
+  function inViewport($el) {
+    var elH = $el.outerHeight(),
+        H   = $(window).height(),
+        r   = $el[0].getBoundingClientRect(), t=r.top, b=r.bottom;
+    return Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H));
+
+  }
+
+
+
+$(window).on("scroll resize", function(){
+  if($(window).width()>990)
+  {
+  var imageStyle = getComputedStyle(bImage);
+  var headerStyle = getComputedStyle(vHeader);
+  var titleStyle = getComputedStyle(title);
+  var aboutStyle = getComputedStyle(about);
+  var closerStyle = getComputedStyle(closer);
+  var linksStyle = getComputedStyle(links)
+
+  var imageWidth = imageStyle.width;
+  var headerWidth = headerStyle.width;
+  var headerHeight = headerStyle.height;
+  var imageMargin = imageStyle.marginRight;
+  var titleMargin = titleStyle.paddingLeft;
+  var titleWidth = titleStyle.width;
+  var aboutMargin = aboutStyle.marginRight;
+  var aboutMarginTop = aboutStyle.marginTop;
+  var aboutMarginBottom = aboutStyle.marginBottom;
+  var closerHeight = closerStyle.height;
+  var linksHeight = linksStyle.height;
+
+  var imageWidthValue = imageWidth.slice(0, -2);
+  var imageMarginValue = imageMargin.slice(0, -2);
+  var headerWidthValue = headerWidth.slice(0, -2);
+  var headerHeightValue = headerHeight.slice(0, -2);
+  var titleMarginValue = titleMargin.slice(0, -2);
+  var titleWidthValue = titleWidth.slice(0, -2);
+  var aboutMarginValue = aboutMargin.slice(0, -2);
+  var aboutMarginTopValue = aboutMarginTop.slice(0, -2);
+  var aboutMarginBottomValue = aboutMarginBottom.slice(0, -2);
+  var closerHeightValue = closerHeight.slice(0, -2);
+  var linksHeightValue = linksHeight.slice(0, -2);
+  var aboutArea = (headerWidthValue - titleMarginValue - imageWidthValue - imageMarginValue - aboutMarginValue)*($(window).height() - headerHeightValue - aboutMarginTopValue - aboutMarginBottomValue - closerHeightValue);
+
+  fade.style.width =  (imageWidthValue*1.2) + "px";
+  filler.style.width = imageMargin;
+  title.style.width = (headerWidthValue - titleMarginValue - imageWidthValue - imageMarginValue) + "px" ;
+  about.style.width = (headerWidthValue - titleMarginValue - imageWidthValue - imageMarginValue - aboutMarginValue) + "px" ;
+  about.style.height = ($(window).height() - headerHeightValue - aboutMarginTopValue - aboutMarginBottomValue - closerHeightValue) + "px";
+  links.style.height = ($(window).height()*0.0852) + "px";
+  about.style.height = ($(window).height() - headerHeightValue - aboutMarginTopValue - aboutMarginBottomValue - closerHeightValue) + "px";
+  if (aboutArea < 291800)
+  {
+    if (Math.floor(aboutArea*0.00007) > 14)
+    {
+    about.style.fontSize = (Math.floor(aboutArea*0.00007)) + "px";
+    }
+    else {
+      about.style.fontSize = "14px";
+    }
+  }
+  else
+  {
+    about.style.fontSize = "20px";
+  }
+
+  if (titleWidthValue > 500)
+    {
+      title.style.fontSize = "100px";
+    }
+  else
+    {
+      title.style.fontSize = (titleWidthValue*0.175) + "px";
+    }
+
+
+links.style.width = 20 + "%";
+
+}
+else{
+  about.style.width = 45 + "%";
+  title.style.fontSize = "180px";
+  title.style.width = 100 + "%";
+  links.style.position = "fixed";
+  links.style.bottom = "40px";
+  links.style.height = "380px";
+}
+
+  });
+
+
+
+
+});
